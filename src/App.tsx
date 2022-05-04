a295e50adbf66c4440b290ea4376433a2cbe74f2
nmcode <nmcode.personal@gmail.com>
2022-05-04 03:09:29 +0300

    start

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="lg">
+      <Grid container spacing={2}>
+        <Grid item xs={8}>
+          <Paper>1</Paper>
+        </Grid>
+        <Grid item xs={4}>
+          <Paper>1</Paper>
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
 
