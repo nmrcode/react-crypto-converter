1df9203dd1853dbb059c6770c783ee4708f84268
nmcode <nmcode.personal@gmail.com>
2022-05-04 03:49:29 +0300

    Добавлено: верстка правого модуля

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,88 @@
-import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
 import React from "react";
+import {
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+  Typography,
+} from "@mui/material";
+
+const paperStyle = {
+  p: 1,
+};
 
 function App() {
   return (
     <Container maxWidth="lg">
-      <Grid container spacing={2}>
+      <Grid container spacing={2} sx={{ mt: 2 }}>
         <Grid item xs={8}>
-          <Paper>1</Paper>
+          <Paper sx={paperStyle}>1</Paper>
         </Grid>
         <Grid item xs={4}>
-          <Paper>1</Paper>
+          <Paper sx={paperStyle}>
+            <Grid container spacing={2} sx={{ mb: 2 }}>
+              <Grid item xs={8}>
+                <TextField
+                  id="outlined-basic"
+                  label="Сумма"
+                  variant="outlined"
+                  fullWidth
+                />
+              </Grid>
+              <Grid item xs={4}>
+                <FormControl fullWidth>
+                  <InputLabel id="demo-simple-select-label">Валюта</InputLabel>
+                  <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    value={0}
+                    label="Age"
+                  >
+                    <MenuItem value={10}>Ten</MenuItem>
+                    <MenuItem value={20}>Twenty</MenuItem>
+                    <MenuItem value={30}>Thirty</MenuItem>
+                  </Select>
+                </FormControl>
+              </Grid>
+            </Grid>
+            <Grid container spacing={2} sx={{ mb: 2 }}>
+              <Grid item xs={8}>
+                <TextField
+                  id="outlined-basic"
+                  label="Сумма"
+                  variant="outlined"
+                  fullWidth
+                />
+              </Grid>
+              <Grid item xs={4}>
+                <FormControl fullWidth>
+                  <InputLabel id="demo-simple-select-label">Валюта</InputLabel>
+                  <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    value={0}
+                    label="Age"
+                  >
+                    <MenuItem value={10}>Ten</MenuItem>
+                    <MenuItem value={20}>Twenty</MenuItem>
+                    <MenuItem value={30}>Thirty</MenuItem>
+                  </Select>
+                </FormControl>
+              </Grid>
+            </Grid>
+            <Typography
+              component="p"
+              fontWeight={600}
+              fontSize={20}
+              textAlign={"center"}
+            >
+              68,25 Российский рубль
+            </Typography>
+          </Paper>
         </Grid>
       </Grid>
     </Container>
